9f6aa2dba5b067efe8515e19e2bea54fa1324825
vladimorozov <vladi.morozov@ok.de>
2024-04-12 10:42:48 +0200

    CoolChainV2.3

--- Kontrolle.py
+++ Kontrolle.py
@@ -1,4 +1,4 @@
-def stationsKontrolle(liste,liste_Fehler): 
+def stationsKontrolle(liste,liste_Fehler,eingabe): 
     for Liste in range (0, len(liste)):
         
         for station in range (0,len(liste[Liste])-2,2):
@@ -12,31 +12,35 @@ def stationsKontrolle(liste,liste_Fehler):
             direction3 = liste[Liste][station+2][4]
 
             ergebnis = liste[Liste][0][2]
-            if ergebnis not in liste_Fehler:
-                if transportStation1!=transportStation2 and transportStation3==transportStation2 :
-                    liste_Fehler.append(ergebnis)
+            if transportStation1!=transportStation2 and transportStation3==transportStation2 and ergebnis not in liste_Fehler:
+                liste_Fehler.append(ergebnis)
+                if eingabe ==1:
                     print("ID: ",ergebnis,"|Aus- oder Eincheck-Zeitpunkt fehlt in der Mitte\n") 
 
-                if transportStation1!=transportStation2 and direction1 == direction2 :
-                    liste_Fehler.append(ergebnis)
+            if transportStation1!=transportStation2 and direction1 == direction2 and ergebnis not in liste_Fehler: 
+                liste_Fehler.append(ergebnis)
+                if eingabe ==1:
                     print("ID: ",ergebnis,"|Fehler bei den Transportstationen\n")  
 
-                if transportStation1==transportStation3 and direction1 == direction3: 
-                    liste_Fehler.append(ergebnis)
+            if transportStation1==transportStation3 and direction1 == direction3 and ergebnis not in liste_Fehler: 
+                liste_Fehler.append(ergebnis)
+                if eingabe ==1:
                     print("ID: ",ergebnis,"|Aus und wieder Einchecken im gleichen Kühllager\n")    
 
-                if transportStation3==transportStation2 and direction3 == direction2: 
-                    liste_Fehler.append(ergebnis)
+            if transportStation3==transportStation2 and direction3 == direction2 and ergebnis not in liste_Fehler: 
+                liste_Fehler.append(ergebnis)
+                if eingabe ==1:
                     print("ID: ",ergebnis,"|Doppelter Auscheck-Zeitpunkt\n")   
 
-                if len(liste[Liste]) % 2 !=0:
-                    liste_Fehler.append(ergebnis)
-                    print("ID: ",ergebnis,"|Auscheck-Zeitpunkt fehlt am Ende da Tr. nicht abgeschlossen.\n")
+        if len(liste[Liste]) % 2 !=0 and ergebnis not in liste_Fehler:
+            liste_Fehler.append(ergebnis)
+            if eingabe ==1:
+                print("ID: ",ergebnis,"|Auscheck-Zeitpunkt fehlt am Ende da Tr. nicht abgeschlossen.\n")
                 
     return liste_Fehler
 
 
-def zeitKuehlung(liste,zeit,wetter,fehler):
+def zeitKuehlung(liste,zeit,wetter,fehler,eingabe):
     
     for Liste in range (0, len(liste)):
     #Kontrolle Zeiträume ohne Kühlung
@@ -46,8 +50,10 @@ def zeitKuehlung(liste,zeit,wetter,fehler):
             transportzeit2 = liste[Liste][iOhne_Kuehlung+1][5]
 
             if  transportzeit2 - transportzeit1 > zeit and liste[Liste][0][2] not in wetter  and liste[Liste][0][2] not in fehler:
-                print("ID:",liste[Liste][0][2],"|Verifikation: Übergabe > 10 min\n")
                 wetter.append(liste[Liste][0][2])
+                if eingabe ==1:
+                    print("ID:",liste[Liste][0][2],"|Verifikation: Übergabe > 10 min\n")
+                
                 
     return liste
     return wetter
